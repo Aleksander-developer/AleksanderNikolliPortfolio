b4c053e77acd36043b6aaf44b7faa8c85e334528
Aleksander-developer <aleksandernikollibusiness@gmail.com>
2025-07-23 17:34:30 +0200

    aggiornamenti Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
     // src/environments/environment.ts
     export const environment = {
       production: false,
-      apiUrl: 'http://localhost:10000/api' // <--- CORREZIONE: Qui dovrebbe essere il tuo backend LOCALE
+      apiUrl: 'https://alex-backend-api.onrender.com/api'
+      // apiUrl: 'http://localhost:10000/api' // <--- CORREZIONE: Qui dovrebbe essere il tuo backend LOCALE
     };
 
